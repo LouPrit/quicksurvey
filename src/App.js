ac664fd56c2de2fe1fb857e08ffa0e989aac1dbc
Louis Pritchard <louispritchard91@hotmail.com>
2019-05-10 13:31:26 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="App-header">
+          <div>
+            <p className="App-title">QuickSurvey</p>
+          </div>
+          <ul className="App-nav">
+            <a href="#">Surveys</a>
+            <a href="#">My Surveys</a>
+            <a href="#">Create Survey</a>
+          </ul>
+          <ul className="App-nav-login">
+            <a href="#">Login</a>
+            <a href="#">Signup</a>
+          </ul>
+        </nav>
+        <div className="App-main">
+
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
