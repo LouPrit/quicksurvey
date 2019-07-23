960b55ef89125b592a67b88afa8b8ad2776bf3ae
Louis Pritchard <louispritchard91@hotmail.com>
2019-07-23 23:00:37 +0100

    Un-ignored build, so it can be pushed to github

---
+++ client/build/precache-manifest.a593666c80619da66fc4a6623b9ea267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc1ae597f69adc7c6c590283b2db674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1b94e156cd27f0c5b9b",
+    "url": "/static/css/main.ccc39623.chunk.css"
+  },
+  {
+    "revision": "b1ea76fde8a9c1129333",
+    "url": "/static/js/2.091725e3.chunk.js"
+  },
+  {
+    "revision": "d1b94e156cd27f0c5b9b",
+    "url": "/static/js/main.b7528774.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
